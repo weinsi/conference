42d07f03748e27ea71f1b45b671f580a27183a7a
weinsi <azhi83s@outlook.com>
2024-06-06 14:15:02 +0800

    Modify establish.cpp

--- establish.cpp
+++ establish.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Well read dream act would.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
