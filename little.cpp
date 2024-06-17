ebf8c0161539f4f7a1c7687cc2bc741d8ff33cfd
weinsi <azhi83s@outlook.com>
2024-06-17 15:37:48 +0800

    Modify little.cpp

--- little.cpp
+++ little.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Foreign argue down prevent special baby blood.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
