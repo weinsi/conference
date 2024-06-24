e03b0a0607c32e288c3e5be32c1ac5f6d01a631a
weinsi <azhi83s@outlook.com>
2024-06-24 11:33:44 +0800

    Modify establish.cpp

--- establish.cpp
+++ establish.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# This red draw tonight trade image.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
