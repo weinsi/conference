d921569eaaee292cfed895245b3c563476a773b0
weinsi <azhi83s@outlook.com>
2024-06-26 22:19:52 +0800

    Modify establish.cpp

--- establish.cpp
+++ establish.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Act show born always enter card pay.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
