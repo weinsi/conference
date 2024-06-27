4e6d1b01700f8c3e58e80e8b650c1aac17baaef8
weinsi <azhi83s@outlook.com>
2024-06-27 22:52:35 +0800

    Modify establish.cpp

--- establish.cpp
+++ establish.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# City assume feel summer this writer state.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
