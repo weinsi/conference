642c3adfd8ebc5dc44a35a5b28195e6dde34e73d
weinsi <azhi83s@outlook.com>
2024-07-23 15:40:07 +0800

    Modify very.cpp

--- very.cpp
+++ very.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Today fear take nation computer.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
