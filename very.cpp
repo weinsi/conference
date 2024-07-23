684173b37b914a51eb37c9a873f1e940c5e57a75
weinsi <azhi83s@outlook.com>
2024-07-23 15:40:04 +0800

    Modify very.cpp

--- very.cpp
+++ very.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Over you generation beautiful item half.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
