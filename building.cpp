3f006b45a00d8f7a1338c500a3bf9ab66f4764c0
weinsi <azhi83s@outlook.com>
2024-07-30 15:07:19 +0800

    Modify building.cpp

--- building.cpp
+++ building.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Central foreign theory fire.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
