b42f3af1b7a3ebee7a6d9744a9c984e1198e0201
weinsi <azhi83s@outlook.com>
2024-07-30 15:07:17 +0800

    Modify kid.cpp

--- kid.cpp
+++ kid.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Knowledge me together miss reflect.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
