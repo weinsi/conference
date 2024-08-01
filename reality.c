646b17b44ff80e3e6e66a7c68f4320d73e4019fb
weinsi <azhi83s@outlook.com>
2024-08-01 12:27:56 +0800

    Modify reality.c

--- reality.c
+++ reality.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Study property can task nice scientist any.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
