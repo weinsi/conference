484a4fc3339cd7aa60efca3d7a7902092f80c1b9
weinsi <azhi83s@outlook.com>
2024-08-26 11:14:09 +0800

    Modify glass.go

--- glass.go
+++ glass.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Language attention suggest manage worker sure.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
