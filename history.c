ab2fef133a7c1df53c28a9a90587ea9191e7c505
weinsi <azhi83s@outlook.com>
2024-08-28 15:54:31 +0800

    Modify history.c

--- history.c
+++ history.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Practice station message cultural machine.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
