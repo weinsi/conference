bfe494c98bf52d6fca8f208607efdbcb9562c722
weinsi <azhi83s@outlook.com>
2024-08-29 14:18:02 +0800

    Modify myself.cpp

--- myself.cpp
+++ myself.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Win name build.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
