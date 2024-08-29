1231770bbfefdb14a7c7450758949958ef646879
weinsi <azhi83s@outlook.com>
2024-08-29 14:18:02 +0800

    Modify myself.cpp

--- myself.cpp
+++ myself.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Particular per everything reflect role keep behavior.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
