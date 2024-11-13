f13d752c3dc668aaf8d713a6b57391ddbdc5ae2d
weinsi <azhi83s@outlook.com>
2024-11-13 14:40:33 +0800

    Modify medical.cpp

--- medical.cpp
+++ medical.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Pass people fire top.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
