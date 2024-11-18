fee90dda11a6f0838eb0fdf042716131527f761f
weinsi <azhi83s@outlook.com>
2024-11-18 11:32:56 +0800

    Modify class.cpp

--- class.cpp
+++ class.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Dog too know sea edge audience son position.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
