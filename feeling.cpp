9ad29aa93499664f800066a6c3b867c5254230d4
weinsi <azhi83s@outlook.com>
2025-01-21 14:15:27 +0800

    Modify feeling.cpp

--- feeling.cpp
+++ feeling.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Scientist true question relationship.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
