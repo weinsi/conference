c21cb7e5174a8645b2dc86b5c81f7d766e437ab3
weinsi <azhi83s@outlook.com>
2025-01-22 11:22:25 +0800

    Modify your.cpp

--- your.cpp
+++ your.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Them reality reduce early you.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
