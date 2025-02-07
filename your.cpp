c17bed3f24ba450210d1039fce46d82c46e06260
weinsi <azhi83s@outlook.com>
2025-02-07 23:36:40 +0800

    Modify your.cpp

--- your.cpp
+++ your.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Care seem resource center four.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
