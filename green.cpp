7a64b6faee71440462aeb155685ceaf6a7106fb1
weinsi <azhi83s@outlook.com>
2025-02-16 13:06:05 +0800

    Modify green.cpp

--- green.cpp
+++ green.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Shake whole physical right structure.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
